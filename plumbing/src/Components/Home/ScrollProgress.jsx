cdf74c9ff5625299990d735683a7d0cfdd1a3d2a
rimsha_bibi <108316811+Rimshashehzadi@users.noreply.github.com>
2026-07-15 23:16:06 -0700

    add

--- plumbing/src/Components/Home/ScrollProgress.jsx
+++ plumbing/src/Components/Home/ScrollProgress.jsx
@@ -36,31 +36,71 @@ const ScrollProgress = () => {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-50">
+    <div
+      className="
+        fixed
+        bottom-4
+        right-4
+        sm:bottom-5
+        sm:right-5
+        md:bottom-6
+        md:right-6
+        lg:bottom-8
+        lg:right-8
+        z-50
+      "
+    >
       <button
         onClick={showTop ? scrollToTop : scrollToBottom}
         className="
-          w-14
-          h-14
+          w-10
+          h-10
+          sm:w-11
+          sm:h-11
+          md:w-12
+          md:h-12
+          lg:w-14
+          lg:h-14
+
           bg-[#13727b]
           text-white
-          rounded-lg
+          rounded-md
+          sm:rounded-lg
+
           flex
           items-center
           justify-center
+
           shadow-xl
           transition-all
           duration-300
+
           hover:bg-[#0f5f66]
-          hover:scale-110
+          hover:scale-105
+          md:hover:scale-110
+
           active:scale-95
         "
         aria-label={showTop ? "Scroll to Top" : "Scroll to Bottom"}
       >
         {showTop ? (
-          <FaArrowUp size={20} />
+          <FaArrowUp
+            className="
+              text-sm
+              sm:text-base
+              md:text-lg
+              lg:text-xl
+            "
+          />
         ) : (
-          <FaArrowDown size={20} />
+          <FaArrowDown
+            className="
+              text-sm
+              sm:text-base
+              md:text-lg
+              lg:text-xl
+            "
+          />
         )}
       </button>
     </div>
